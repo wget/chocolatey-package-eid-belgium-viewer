784968f8fc29b68cf2ba87fe0949767b8c0272f8
William Gathoye <william@gathoye.be>
2021-02-10 20:44:42 +0100

    Handle different new upstream URLs changes

--- update.ps1
+++ update.ps1
@@ -19,45 +19,71 @@ function global:au_SearchReplace {
     }
   }
 }
+function Get-VersionUrl($tags, $versionPatterns, $baseURLs, $filenamePatterns, $filenameArchPatterns) {
+  foreach ($tag in $tags) {
+    foreach ($versionPattern in $versionPatterns) {
+      $version = $tag.Name -Replace $versionPattern
+      foreach ($baseUrl in $baseUrls) {
+        foreach ($filename in $filenamePatterns) {
+          foreach ($archPattern in $filenameArchPatterns) {
+            try {
+              $baseUrl = $($baseUrl -Replace "\[VERSION\]","$version" -Replace "\[ARCH\]","$archPattern")
+              $url = $($filename -Replace "\[VERSION\]","$version" -Replace "\[ARCH\]","$archPattern")
+              $url = "$($baseUrl)$($url)"
+              Write-Host "Checking: $url"
+              Invoke-WebRequest -Uri $url -UseBasicParsing -DisableKeepAlive -Method HEAD | Out-Null
+              $versionUrl = @{}
+              $versionUrl.version = $version
+              $versionUrl.url = $url
+              return $versionUrl
+            } catch [Net.WebException] {
+            }
+          }
+        }
+      }
+    }
+  }
+  return $null
+}
 
 function global:au_GetLatest {
+
+  $tagsUrl = "https://api.github.com/repos/fedict/eid-mw/tags"
+  $releaseNotesFilenamePatterns = @(
+    "rn[VERSION].pdf",
+    "RN[version].pdf"
+  )
+  $releaseNotesVersionPatterns = @(
+    '[^0-9.]',
+    '[^0-9]'
+  )
+  $errorMessage = "[PREFIX]This shouldn't happen. Upstream has likely changed their URLs, manual intervention required."
 
   $url = "https://eid.belgium.be/en/download/41/license"
   $content = Invoke-WebRequest -Uri $url -UseBasicParsing
   foreach ($i in $content.Links.href) {
     if ($i.endswith("msi")) {
-      $viewerUrl = $i.trim()
+      [System.Uri]$viewerUrl = $i.trim()
       break;
     }
   }
-  
   if (!$viewerUrl) {
-    throw "The viewer URL is empty. Maybe upstream changed their URLs?"
+    throw $errorMessage -Replace "\[PREFIX\]","The viewer URL was not found. "
   }
   
-  $version = $viewerUrl.split('_')[1].replace(".msi","")
+  $version = Split-Path $viewerUrl.LocalPath -leaf
+  $version = ($version -Replace '[^0-9.]').trim('.')
   
   # Determine release notes URL
-  $tags = Invoke-WebRequest -Uri 'https://api.github.com/repos/fedict/eid-mw/tags' -UseBasicParsing | ConvertFrom-Json
-  foreach ($tag in $tags) {
-    $urlReleaseNotes = "https://eid.belgium.be/sites/default/files/content/pdf/rn$($tag.Name -Replace '[a-zA-Z._]*','').pdf"
-    try {
-        Write-Host "Checking: $urlReleaseNotes"
-        (Invoke-WebRequest -Uri $urlReleaseNotes -UseBasicParsing -DisableKeepAlive -Method HEAD).StatusCode
-        break
-    } catch [Net.WebException] {
-        [int]$_.Exception.Response.StatusCode
-        continue
-    }
-    if (!$urlReleaseNotes) {
-      throw "The URL to the release notes was not found. This shouldn't happen. Maybe upstream changed their URLs?"
-    }
+  $versionUrlReleaseNotes = Get-VersionUrl $tags $releaseNotesVersionPatterns $releaseNotesBaseUrls $releaseNotesFilenamePatterns @{}
+  if (!$versionUrlReleaseNotes) {
+    throw $errorMessage -Replace "\[PREFIX\]","The URL to the release notes was not found. "
   }
 
   return @{
-    URL32 = $viewerUrl
+    URL32 = $viewerUrl.AbsoluteUri
     Version = $version
-    ReleaseNotes = $urlReleaseNotes
+    ReleaseNotes = $versionUrlReleaseNotes.url
   }
 }
 
